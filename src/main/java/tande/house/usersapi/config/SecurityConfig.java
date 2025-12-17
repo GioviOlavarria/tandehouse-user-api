4743bc813d7e185d9a6370e958fdec1c9ee202d4
Giovanni Olavarría <gi.olavarria@duocuc.cl>
2025-12-17 03:03:07 -0300

    Solución error cors

--- src/main/java/tande/house/usersapi/config/SecurityConfig.java
+++ src/main/java/tande/house/usersapi/config/SecurityConfig.java
@@ -29,8 +29,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/auth/login", "/auth/register").permitAll()
-                        .requestMatchers("/auth/internal/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
