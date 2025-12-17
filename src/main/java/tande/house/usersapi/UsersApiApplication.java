2c1bbbe9921307139bce9e8abf5ca1a936a83a5d
Giovanni Olavarría <gi.olavarria@duocuc.cl>
2025-12-17 03:38:00 -0300

    cambio cors

--- src/main/java/tande/house/usersapi/UsersApiApplication.java
+++ src/main/java/tande/house/usersapi/UsersApiApplication.java
@@ -3,8 +3,11 @@ package tande.house.usersapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "tande.house.usersapi")
+@SpringBootApplication(scanBasePackages = {
+        "tande.house.usersapi"
+})
 public class UsersApiApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(UsersApiApplication.class, args);
     }
